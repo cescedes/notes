cd792c51eaa3f2bb0994b7b88b33a144b91e8125
aybüke <77280855+cescedes@users.noreply.github.com>
2024-03-18 04:47:09 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,3 +1,14 @@
+#a function called password_generator() that takes two inputs, first_name and last_name, 
+# and then concatenates the last three letters of each and returns them as a string.
+
+def password_generator(first_name, last_name):
+  password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return password
+
+temp_password = password_generator("Reiko", "Matsuki")
+
+
+
 # a function called contains that takes two arguments, big_string and little_string
 # and returns True if big_string contains little_string.
 
