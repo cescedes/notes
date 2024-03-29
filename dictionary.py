fbc3bc8334c226408586a32c31fd72938fd37f39
aybüke <77280855+cescedes@users.noreply.github.com>
2024-03-29 21:00:14 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -23,7 +23,8 @@ drinks_to_caffeine = {key:value for key, value in zipped_drinks}
 songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
 playcounts = [78, 29, 44, 21, 89, 5]
 
-# Using a dict comprehension, create a dictionary called plays that goes through zip(songs, playcounts) and creates a song:playcount pair for each song in songs and each playcount in playcounts.
+# Using a dict comprehension, create a dictionary called plays that goes through zip(songs, playcounts) 
+# and creates a song:playcount pair for each song in songs and each playcount in playcounts.
 plays = {key:value for key, value in zip(songs, playcounts)}
 
 print(plays)
@@ -35,8 +36,16 @@ plays["Purple Haze"] = 1
 # update 
 plays["Respect"] = 94
 
-#Create a dictionary called library that has two key: value pairs: key "The Best Songs" with a value of plays, the dictionary you created, key "Sunday Feelings" with a value of an empty dictionary
+# Create a dictionary called library that has two key: value pairs: 
+# key "The Best Songs" with a value of plays, the dictionary you created, key "Sunday Feelings" with a value of an empty dictionary
 library ={"The Best Songs": plays, "Sunday Feelings": {}}
 
 print(library)
 #{'The Best Songs': {'Like a Rolling Stone': 78, 'Satisfaction': 29, 'Imagine': 44, "What's Going On": 21, 'Respect': 94, 'Good Vibrations': 5, 'Purple Haze': 1}, 'Sunday Feelings': {}}
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+# get the key
+print(zodiac_elements["earth"])
+#['Taurus', 'Virgo', 'Capricorn']
+print(zodiac_elements["fire"])
+#['Aries', 'Leo', 'Sagittarius']
