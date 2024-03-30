35dc7604b5afb87631f8145055eb259b5b6cb466
aybüke <77280855+cescedes@users.noreply.github.com>
2024-03-30 03:53:36 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -49,3 +49,37 @@ print(zodiac_elements["earth"])
 #['Taurus', 'Virgo', 'Capricorn']
 print(zodiac_elements["fire"])
 #['Aries', 'Leo', 'Sagittarius']
+
+# check if a key exists
+if 'energy' in zodiac_elements:
+  print(zodiac_elements["energy"])
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+#.get() method to search for a value, 
+# you can also specify a value to return if the key doesn’t exist
+tc_id = user_ids.get('teraCoder', 100000)
+print(tc_id)
+#100019
+
+#.pop() works to delete items from a dictionary, when you know the key value
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+# In one line, add the corresponding value of the key "stamina grains" to the health_points variable
+# and remove the item "stamina grains" from the dictionary.
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+# built-in list() function return only keys
+#.keys() method returns a dict_keys object
+# A dict_keys object is a view object, which provides a look at the current state of the dictionary,
+# without the user being able to modify anything
+# but it can be used in the place of a list for iteration
+users = user_ids.keys()
+print(users)
+#dict_keys(['teraCoder', 'pythonGuy', 'samTheJavaMaam', 'lyleLoop', 'keysmithKeith'])
+lessons = num_exercises.keys()
+print(lessons)
+#dict_keys(['functions', 'syntax', 'control flow', 'loops', 'lists', 'classes', 'dictionaries'])
+
