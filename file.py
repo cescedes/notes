64ddd4df9cc9c14a90b53e25514c1d3b3c81dadf
aybüke <77280855+cescedes@users.noreply.github.com>
2024-04-04 16:21:35 +0200

    Update file.py

--- file.py
+++ file.py
@@ -78,3 +78,51 @@ data_payload = [
 import json
 with open('data.json', 'w') as data_json:
   json.dump(data_payload, data_json)
+
+#####################
+
+# 'hacking the fender' from codecademy
+
+import csv
+compromised_users = []
+with open("passwords.csv") as password_file:
+  password_csv = csv.DictReader(password_file)
+  #save each row of the csv into password_row
+  #add each username to the list compromised_users
+  for row in password_csv:
+    password_row = row
+    compromised_users.append(row)
+
+#iterating over each compromised_users and write the username of each compromised_user in comromised_users to compromised_user_file
+with open("compromised_users.txt", "w") as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(str(user) + '\n')
+
+import json
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
+  #write out boss_message_dict to boss_message
+  json.dump(boss_message_dict, boss_message)
+
+#replace passwords.csv with the following new file
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  slash_null_sig = """ 
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+
+"""
+  new_passwords_obj.write(slash_null_sig)
