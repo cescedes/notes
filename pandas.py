3bfb4ea79b8e32234f551d0f8a215e1692558763
aybüke <77280855+cescedes@users.noreply.github.com>
2024-04-06 00:43:34 +0200

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+#create a dataframe
+df1 = pd.DataFrame({
+  'Product ID': [1, 2, 3, 4],
+  'Product Name':['t-shirt', 't-shirt', 'skirt', 'skirt'],
+  'Color': ['blue', 'green', 'red', 'black']
+})
+
+#create a dataframe w/ keyword columns
+df2 = pd.DataFrame([
+  [1, 'San Diego', 100],
+  [2, 'Los Angeles', 120],
+  [3, 'San Francisco', 90],
+  [4, 'Sacramento', 115]
+],
+  columns=[
+    'Store ID', 'Location', 'Number of Employees'
+  ])
+
+#read csv
+df = pd.read_csv('sample.csv')
+
+#load the CSV:
+df = pd.read_csv('imdb.csv')
+print(df.head())
+print(df.info())
+
+########
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+
+clinic_north = df['clinic_north']
+print(type(clinic_north)) #<class 'pandas.core.series.Series'>
+print(type(df)) #<class 'pandas.core.frame.DataFrame'>
+
+#########
+
