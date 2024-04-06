29d374a05139d1014aaea8194671e5e855704e7a
aybüke <77280855+cescedes@users.noreply.github.com>
2024-04-07 01:50:04 +0200

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -43,5 +43,44 @@ clinic_north = df['clinic_north']
 print(type(clinic_north)) #<class 'pandas.core.series.Series'>
 print(type(df)) #<class 'pandas.core.frame.DataFrame'>
 
-#########
+#select multiple columns
+clinic_north_south = df[['clinic_north', 'clinic_south']]
+print(type(clinic_north_south)) #<class 'pandas.core.frame.DataFrame'>
 
+#select row
+march = df.iloc[2]
+#When we select a single row, the result is a Series (just like when we select a single column).
+#select multiple rows
+april_may_june = df.iloc[3:6]
+
+#select rows with logic
+january = df[df.month == 'January']
+march_april = df[(df.month == 'March') | (df.month == 'April')]
+# select rows with logic, the isin command to check that df.month is one of a list of values:
+january_february_march = df[df.month.isin(['January', 'February', 'March'])]
+
+#When we select a subset of a DataFrame using logic, we end up with non-consecutive indices. 
+#This is inelegant and makes it hard to use .iloc().
+#We can fix this using the method .reset_index().
+df2 = df.loc[[1, 3, 5]]  #the subset with indexes 1,3,5
+df3 = df2.reset_index()
+#the old indices have been moved into a new column called 'index'. 
+# Unless you need those values for something special, 
+#it’s probably better to use the keyword drop=True so that you don’t end up with that extra column. 
+#If we run the command df.reset_index(drop=True), we get a new DataFrame
+#If we use the keyword inplace=True we can just modify our existing DataFrame.
+df2.reset_index(inplace = True, drop = True)
+
+##################
+
+#Load the data from shoefly.csv into the variable orders.
+orders = pd.read_csv('shoefly.csv')
+#Inspect the first 5 lines of the data.
+print(orders.head())
+#Select all of the email addresses from the column email and save them to a variable called emails.
+emails = orders['email']
+#Frances Palmer claims that her order was wrong. What did Frances Palmer order?
+#Use logic to select that row of orders and save it to the variable frances_palmer.
+frances_palmer = orders[(orders.first_name == 'Frances') & (orders.last_name == 'Palmer')]
+#Select all orders for shoe_type: clogs, boots, and ballet flats and save them to the variable comfy_shoes.
+comfy_shoes = orders[(orders.shoe_type == 'clogs') | (orders.shoe_type == 'boots') | (orders.shoe_type == 'ballet flats')]
