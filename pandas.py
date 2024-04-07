0e4906b0f28bc14d7a1edd8aa9b16438112041f2
aybüke <77280855+cescedes@users.noreply.github.com>
2024-04-08 00:00:26 +0200

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -141,3 +141,27 @@ orders['shoe_source'] = orders.shoe_material.apply(lambda x: 'vegan' if x != 'le
 #Using the columns last_name and gender create a column called salutation which contains Dear Mr. <last_name> for men and Dear Ms. <last_name> for women.
 orders['salutation'] = orders.apply(lambda row: 'Dear Mr. ' + row['last_name'] if row['gender'] == 'male' else 'Dear Ms. ' + row['last_name'], axis=1)
 
+###################
+
+import pandas as pd
+#Data for all of the locations of Petal Power is in the file inventory.csv. Load the data into a DataFrame called inventory.
+inventory = pd.read_csv('inventory.csv')
+#Inspect the first 10 rows of inventory.
+print(inventory.head(10))
+#The first 10 rows represent data from your Staten Island location. Select these rows and save them to staten_island.
+staten_island = inventory.iloc[:10]
+#A customer just emailed you asking what products are sold at your Staten Island location. 
+#Select the column product_description from staten_island and save it to the variable product_request.
+product_request = staten_island['product_description']
+#Another customer emails to ask what types of seeds are sold at the Brooklyn location.
+#Select all rows where location is equal to Brooklyn and product_type is equal to seeds and save them to the variable seed_request.
+seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
+#Add a column to inventory called in_stock which is True if quantity is greater than 0 and False if quantity equals 0.
+inventory['in_stock'] = inventory.apply(lambda row: True if row.quantity > 0 else False, axis=1)
+#Create a column called total_value that is equal to price multiplied by quantity.
+inventory['total_value'] = inventory['price'] * inventory['quantity']
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+#Using combine_lambda, create a new column in inventory called full_description that has the complete description of each product.
+inventory['full_description'] = inventory.apply(combine_lambda, axis=1)
