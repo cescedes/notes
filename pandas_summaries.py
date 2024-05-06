37f6f293d6be9ba7fa7cf9b6c602a175f4200f1d
aybüke <77280855+cescedes@users.noreply.github.com>
2024-05-06 02:04:22 +0200

    Update pandas_summaries.py

--- pandas_summaries.py
+++ pandas_summaries.py
@@ -44,3 +44,36 @@ print(std_budget)
 
 # Save the mean absolute deviation to mad_budget
 mad_budget = movies.production_budget.mad()
+
+######################
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+movies = pd.read_csv('movies.csv')
+
+# Create a boxplot for movie budget 
+sns.boxplot(x='production_budget', data=movies)
+plt.show()
+plt.close()
+
+# Create a histogram for movie budget
+sns.histplot(x='production_budget', data=movies)
+plt.show()
+plt.close()
+
+# Save the counts to genre_counts
+genre_counts = movies.genre.value_counts()
+
+# Save the proportions to genre_props
+genre_props = movies.genre.value_counts() / len(movies.genre)
+
+# Create a bar chart for movie genre 
+sns.countplot(x='genre', data=movies)
+plt.show()
+plt.close()
+
+# Create a pie chart for movie genre
+movies.genre.value_counts().plot.pie()
+plt.show()
+plt.close()
