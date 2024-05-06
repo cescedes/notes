41ab7f8bb5c157371e38b02bc9042acf76c774ef
aybüke <77280855+cescedes@users.noreply.github.com>
2024-05-06 15:06:25 +0200

    Update pandas_summaries.py

--- pandas_summaries.py
+++ pandas_summaries.py
@@ -77,3 +77,52 @@ plt.close()
 movies.genre.value_counts().plot.pie()
 plt.show()
 plt.close()
+
+
+#############################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import data
+students = pd.read_csv('students.csv')
+# Print first few rows of data
+print(students.head())
+# Print summary statistics for all columns
+print(students.describe(include = 'all'))
+# Calculate mean
+print(students.math_grade.mean())
+# Calculate median
+print(students.math_grade.median())
+# Calculate mode
+print(students.math_grade.mode()[0])
+# Calculate range
+math_range = students.math_grade.max() - students.math_grade.min()
+print(math_range)
+# Calculate standard deviation
+print(students.math_grade.std())
+#The standard deviation is about 4.6, while the average grade is about 10.4. 
+#This means that about two thirds of students are earning a grade between 5.8 (calculated as 10.4 - 4.6) and 15 (calculated as 10.4 + 4.6).
+# Calculate MAD
+print(students.math_grade.mad())
+# Create a histogram of math grades
+sns.histplot(x='math_grade', data=students)
+plt.show()
+plt.clf()
+# Create a box plot of math grades
+sns.boxplot(x='math_grade', data=students)
+plt.show()
+plt.clf()
+# Calculate number of students with mothers in each job category
+print(students.Mjob.value_counts())
+# Calculate proportion of students with mothers in each job category
+print(students.Mjob.value_counts(normalize=True))
+# Create bar chart of Mjob
+sns.countplot(x='Mjob', data=students)
+plt.show()
+plt.clf()
+# Create pie chart of Mjob
+students.Mjob.value_counts().plot.pie()
+plt.show()
