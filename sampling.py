a1634edac1d6f15b832fdd4e2501afd640222cf3
aybüke <77280855+cescedes@users.noreply.github.com>
2024-12-11 18:54:28 +0100

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -52,3 +52,94 @@ sns.histplot(sample_means,stat='density')
 plt.title("Sampling Distribution of the Mean")
 plt.xlabel("Weight (lbs)")
 plt.show()
+
+########
+
+cod_population = pd.read_csv("cod_population.csv")
+# Save transaction times to a separate numpy array
+population = cod_population['Cod_Weight']
+
+sns.histplot(population, stat = 'density' )
+plt.title("Population Distribution")
+plt.show()
+
+sample_means = []
+
+# Below is our sample size
+samp_size = 50
+
+for i in range(500):
+    samp = np.random.choice(population, samp_size, replace = False)
+    this_sample_mean = np.mean(samp)
+    sample_means.append(this_sample_mean)
+
+plt.clf() # this closes the previous plot
+sns.histplot(sample_means, stat = 'density' )
+plt.title("Sampling Distribution of the Mean")
+plt.xlabel("Weight (lbs)")
+plt.show()
+
+## when increased the sample size, the sampling distribution look more normal.
+
+###
+# when the original population is normally distributed, the CLT applies even with a smaller sample size.
+###
+###### Calculating Probabilities #######
+## Setting up our parameters
+std_dev = 20
+samp_size = 25
+standard_error = std_dev / (samp_size**.5)
+# we can use the normal CDF to calculate the probability that a sample of 25 fish has a mean weight of 30 lbs.
+x=750/25 # Because we want 25 fish to weigh less than 750 lbs, we want our sample mean to be 750/25, or 30 lbs.
+mean = 36
+cod_cdf = stats.norm.cdf(x, mean, standard_error)
+#The probability of 25 fish having an average weight of 30 or less is 10%. Based on this, it’s probably a good idea to put less fish in the crate or to get a sturdier crate.
+
+###
+
+#In order to review, let’s consider an example from a restaurant serving quarter-pounder burgers. Their quarter-pounders weigh an average of 0.25 lbs with a standard deviation of 0.2 lbs.
+
+#Let’s say we weigh all their burgers that they cook for dinner on a given night. 64 people order quarter-pounders. What is the probability that the mean will be 0.24 lbs or less?
+# 0.345
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+# Set up parameters here:
+x = 0.24
+population_mean = 0.25
+population_std_dev = 0.2
+samp_size = 64
+
+### Below is code to create simulated dataset and calculate Standard Error
+standard_error = population_std_dev / (samp_size**.5)
+
+this_cdf = round(stats.norm.cdf(x,population_mean,standard_error),3)
+
+# Create the population
+population = np.random.normal(population_mean, population_std_dev, size = 100000)
+
+# Simulate the sampling distribution
+sample_means = []
+for i in range(500):
+    samp = np.random.choice(population, samp_size, replace = False)
+    sample_means.append(np.mean(samp))
+
+mean_sampling_distribution = round(np.mean(sample_means),3)
+std_sampling_distribution = round(np.std(sample_means),3)
+
+std_error = population_std_dev / (samp_size **0.5)
+
+sns.histplot(population, stat = 'density')
+plt.title(f"Population Mean: {population_mean} \n Population Std Dev: {population_std_dev} \n Standard Error = {population_std_dev} / sq rt({samp_size}) \n Standard Error = {std_error} ")
+plt.xlabel("")
+plt.show()
+plt.clf()
+
+# Plot the sampling distribution
+sns.histplot(sample_means, stat = 'density')
+plt.axvline(x,color='r',linestyle='dashed')
+plt.title(f"Sampling Dist Mean: {mean_sampling_distribution} \n Sampling Dist Standard Deviation: {std_sampling_distribution}\n CDF for x={x}: {this_cdf}")
+plt.xlabel("")
+plt.show()
